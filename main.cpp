07b68b4969e803c750c04d36d395898a4054e481
huangguifei <xiaohuihuihuige@163.com>
2022-01-17 15:02:40 +0800

    next commit

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
-#include "mainwindow.h"
+﻿#include "mainwindow.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    MainWindow m;
+    m.show();
 
     return a.exec();
 }
